5de66bdfaba52621004aa68b5cd2a4a8f0ad08da
kyak <bas@bmail.ru>
2010-11-13 10:44:07 +0300

    ben-cyrillic: add the required LC_ALL env variable

--- ben-cyrillic/files/russian.sh
+++ ben-cyrillic/files/russian.sh
@@ -2,3 +2,10 @@
 setfont2 /usr/share/ben-cyrillic/un-fuzzy-6x10-font_rus.pnm
 loadkeys /usr/share/ben-cyrillic/ben_ru_uni.map
 loadunimap /usr/share/ben-cyrillic/ben_ru_uni.trans
+profile="/etc/profile"
+locale="export LC_ALL=ru_RU.UTF-8"
+grep "$locale" "$profile" > /dev/null 2>&1
+if [ "$?" -eq "1" ]; then
+	#presumably first run, need to set the LC_ALL env
+	echo "$locale" >> "$profile"
+fi
