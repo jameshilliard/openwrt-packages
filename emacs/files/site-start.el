f00fc2352f8e75fd5a9bfaadff86b814d4c67a05
David Kühling <dvdkhlng@gmx.de>
2010-12-13 10:55:08 +0100

    added emacs startup file for configuring stuff specific to the openwrt package

---
+++ emacs/files/site-start.el
@@ -0,0 +1,11 @@
+;; Emacs startup file for OpenWRT Emacs package
+;;
+;; Copyright (C) 2010 David Kuehling  <dvdkhlng TA gmx TOD de>
+;;
+
+;; load documentation for internal functions.  This is skipped by loadup.el
+;; when not dumping so we do it here.
+(Snarf-documentation "DOC")
+
+;; Allow us to output internatinal characters to the terminal
+(set-terminal-coding-system 'utf-8)
